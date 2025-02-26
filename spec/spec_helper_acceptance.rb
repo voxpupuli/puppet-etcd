244a53c96f0c740a34116df79818ec961883ec1b
Tim Meusel <tim@bastelfreak.de>
2025-02-26 14:13:18 +0100

    modulesync: manage spec_helper_acceptance

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,22 +1,10 @@
 # frozen_string_literal: true
 
-require 'beaker-rspec'
-require 'beaker-puppet'
-require 'beaker/puppet_install_helper'
-require 'beaker/module_install_helper'
+# Managed by modulesync - DO NOT EDIT
+# https://voxpupuli.org/docs/updating-files-managed-with-modulesync/
 
-dir = __dir__
-Dir["#{dir}/acceptance/shared_examples/**/*.rb"].sort.each { |f| require f }
-require 'spec_helper_acceptance_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_acceptance_local.rb'))
+require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-run_puppet_install_helper
-install_module_on(hosts)
-install_module_dependencies_on(hosts)
+configure_beaker(modules: :metadata)
 
-RSpec.configure do |c|
-  # Readable test descriptions
-  c.formatter = :documentation
-end
-
-require 'spec_helper_acceptance_setup' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_acceptance_setup.rb'))
-# 'spec_overrides' from sync.yml will appear below this line
+Dir['./spec/support/acceptance/**/*.rb'].sort.each { |f| require f }
